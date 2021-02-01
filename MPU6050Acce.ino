46327fc69ebbc52a0407479d6754a6411d6fce5a
Tayyaba30 <47871421+Tayyaba30@users.noreply.github.com>
2021-02-01 10:23:47 -0800

    Update MPU6050Acce.ino

--- MPU6050Acce.ino
+++ MPU6050Acce.ino
@@ -1,3 +1,10 @@
+/*
+MPU6050.cpp - Class file for the MPU6050 Triple Axis Gyroscope & Accelerometer Arduino Library.
+Version: 1.0.3
+(c) 2014-2015 Korneliusz Jarzebski
+www.jarzebski.pl
+*/
+
 #include <Servo.h>
 
 #include <MPU6050.h>
